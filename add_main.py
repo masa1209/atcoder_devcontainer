419f9389940291e32eea8731f248daa5ab327702
masa1209 <pleasewaitthere@gmail.com>
2023-03-04 05:08:39 +0000

    add add_main.py

---
+++ add_main.py
@@ -0,0 +1,18 @@
+import glob
+import os
+
+print("target_dir")
+current_dir = input()
+
+dir_list = glob.glob(current_dir + "/*")
+
+item_list = ["a","b","c","d","e","f","g","e","ex"]
+
+for i in dir_list:
+    target_dir = i.split("/")[-1]
+    print(type(target_dir))
+    if target_dir in item_list:
+        f = open(i+"/main.py", 'w')
+        f.write('')
+        f.close()
+        
